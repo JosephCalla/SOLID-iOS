b94ee23dc050e350501265d7f9f07fde30c1683e
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-11-20 19:26:15 -0500

    Open-Closed Principle

--- ExpenseTracker/Enums/ReportRange.swift
+++ ExpenseTracker/Enums/ReportRange.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2022 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -30,58 +30,23 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import CoreData
-import Combine
+import Foundation
 
-class DailyReportsDataSource: ObservableObject {
-  var viewContext: NSManagedObjectContext
+enum ReportRange: String, CaseIterable {
+case daily = "Today"
+case weekly = "This Week"
+case montly = "This Month"
 
-  @Published private(set) var currentEntries: [ExpenseModel] = []
-
-  init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
-    self.viewContext = viewContext
-    prepare()
-  }
-
-  func prepare() {
-    currentEntries = getEntries()
-  }
-
-  private func getEntries() -> [ExpenseModel] {
-    let fetchRequest: NSFetchRequest<ExpenseModel> = ExpenseModel.fetchRequest()
-    fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \ExpenseModel.date, ascending: false)]
-    fetchRequest.predicate = NSPredicate(
-      format: "%@ <= date AND date <= %@",
-      Date().startOfDay as CVarArg,
-      Date().endOfDay as CVarArg)
-    do {
-      let results = try viewContext.fetch(fetchRequest)
-      return results
-    } catch let error {
-      print(error)
-      return []
+  func timeRange() -> (Date, Date) {
+    let now = Date()
+    switch self {
+    case .daily:
+      return (now.startOfDay, now.endOfDay)
+    case .weekly:
+      return (now.startOfWeek, now.endOfWeek)
+    case .montly:
+      return (now.startOfMonth, now.endOfMonth)
     }
   }
-
-  func saveEntry(title: String, price: Double, date: Date, comment: String) {
-    let newItem = ExpenseModel(context: viewContext)
-    newItem.title = title
-    newItem.date = date
-    newItem.comment = comment
-    newItem.price = price
-    newItem.id = UUID()
-
-    if let index = currentEntries.firstIndex(where: { $0.date ?? Date() < date }) {
-      currentEntries.insert(newItem, at: index)
-    } else {
-      currentEntries.append(newItem)
-    }
-
-    try? viewContext.save()
-  }
-
-  func delete(entry: ExpenseModel) {
-    viewContext.delete(entry)
-    try? viewContext.save()
-  }
+  
 }
--- ExpenseTracker/Views/ExpensesView.swift
+++ ExpenseTracker/Views/ExpensesView.swift
@@ -33,9 +33,9 @@
 import SwiftUI
 import Combine
 
-struct DailyExpensesView: View {
+struct ExpensesView: View {
   @State private var isAddPresented = false
-  @ObservedObject var dataSource: DailyReportsDataSource
+  @ObservedObject var dataSource: ReportsDataSource
 
   var body: some View {
     VStack {
@@ -65,10 +65,10 @@ struct DailyExpensesView: View {
   }
 }
 
-struct DailyExpensesView_Previews: PreviewProvider {
+struct ExpensesView_Previews: PreviewProvider {
   static var previews: some View {
-    let reportsDataSource = DailyReportsDataSource(
-      viewContext: PersistenceController.preview.container.viewContext)
-    DailyExpensesView(dataSource: reportsDataSource)
+    let reportsDataSource = ReportsDataSource(viewContext: PersistenceController.preview.container.viewContext,
+                                              reportRange: .daily)
+    ExpensesView(dataSource: reportsDataSource)
   }
 }
